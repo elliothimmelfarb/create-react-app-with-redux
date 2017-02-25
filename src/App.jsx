2ce9e23f2a37873f536c477a7b0a8905f7f2016b
elliothimmelfarb <elliothimmelfarb@gmail.com>
2017-02-25 14:23:59 -0700

    changed site title and welcome message to reflect whats included in this starter

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ class App extends Component { // eslint-disable-line react/prefer-stateless-func
       <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+          <h2>Welcome to React-Redux</h2>
         </div>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
